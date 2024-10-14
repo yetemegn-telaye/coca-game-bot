6002a6af2ab53dc59073f3ad7690ed5c851927fd
yetemegn-telaye <75949338+yetemegn-telaye@users.noreply.github.com>
2024-10-14 19:44:57 +0300

    fix canvas radius

--- src/features/mainGame/index.jsx
+++ src/features/mainGame/index.jsx
@@ -11,7 +11,7 @@ const GameScreen = () => {
     type: Phaser.AUTO,
     parent: 'game-container',
     width: 320,
-    height: 600,
+    height: 558,
    
     scene: new GameScene({ setScore }),
     physics: {
@@ -20,13 +20,17 @@ const GameScreen = () => {
         gravity: { y: 0 },
       },
     },
+    scale: {
+      mode: Phaser.Scale.RESIZE, // Adjust Phaser to resize according to container
+      autoCenter: Phaser.Scale.CENTER_BOTH, // Center the game
+    },
   };
 
 
 
 
   return (
-    <div className='main-container'>
+    <div>
       <MainGame config={config} />
     </div>
   );
