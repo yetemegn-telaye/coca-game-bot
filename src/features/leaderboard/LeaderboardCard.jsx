b0a10dd44522287f5e5371bf403c699d4f1a2a9e
yetemegn-telaye <75949338+yetemegn-telaye@users.noreply.github.com>
2024-10-15 14:50:16 +0300

    fix card

--- src/features/leaderboard/LeaderboardCard.jsx
+++ src/features/leaderboard/LeaderboardCard.jsx
@@ -2,7 +2,7 @@ import CoinCapIcon from '../../assets/icons/coca-cola-cap2-removebg-preview 1.sv
 
 const LeaderboardCard = ({player})=>{
     return(
-        <div className={`bg-white text-black text-sm flex justify-between py-1 px-2 rounded-3xl  ${player.place === '4' ? 'border border-2 border-primary' : '' }`}>
+        <div className={`bg-white text-black text-sm flex justify-between py-1 px-2 rounded-3xl  ${player.place === '4' ? 'border border-2 border-primary text-primary' : '' }`}>
             <div className="flex items-center gap-2">
             <p>{player.place}</p>
             <img src={player.playerImg} alt="player" className="w-10 h-10 rounded-full" />
