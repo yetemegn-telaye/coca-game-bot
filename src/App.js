85e4b18cc729f4e80384a00635f8dfc368ec4397
yetemegn-telaye <75949338+yetemegn-telaye@users.noreply.github.com>
2024-10-15 11:52:44 +0300

    fix styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GameScreen from './features/mainGame';
 import MainMenu from './features/mainMenu';
+import LeaderBoard from './features/leaderboard';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
       <Route path="/" element={<MainMenu />} />
+      <Route path="/leaderboard" element={<LeaderBoard/>}/>
       <Route path="/game-screen" element={<GameScreen />} />
     </Routes>
     </BrowserRouter>
---
+++ src/features/leaderboard/index.jsx
@@ -0,0 +1,8 @@
+const LeaderBoard = ()=>{
+    return(
+        <div className="main-container bg-gradient-primary">
+            leaderboard
+        </div>
+    )
+}
+export default LeaderBoard;
