c1c2043dd9e583a1d9aab245e7dce8288e5e3921
yetemegn-telaye <75949338+yetemegn-telaye@users.noreply.github.com>
2024-10-16 16:20:35 +0300

    Implement link for buttons and fix topbar

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,11 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {  faChevronLeft, faGear} from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
 const TopBar = ({title}) => {  
+    const navigate = useNavigate();
     return (
-        <div className="flex justify-between items-center px-5 py-4">
+        <div className="bg-primary w-full flex justify-between rounded-t-xl items-center px-5 py-3">
             <div className='flex items-center gap-2'>
-            <FontAwesomeIcon icon={faChevronLeft} />
+            <FontAwesomeIcon icon={faChevronLeft} className='hover:cursor-pointer hover:text-black' onClick={()=>{navigate(-1)}}/>
             <p className='text-sm font-bold'>{title}</p>
             </div>
             <FontAwesomeIcon icon={faGear}/>
