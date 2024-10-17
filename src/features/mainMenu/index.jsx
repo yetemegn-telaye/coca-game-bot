512cddbccce0e99f680613dd4168ce3e436d8555
yetemegn-telaye <75949338+yetemegn-telaye@users.noreply.github.com>
2024-10-17 16:37:35 +0300

    Setup telegram bot

--- src/features/mainMenu/index.jsx
+++ src/features/mainMenu/index.jsx
@@ -1,4 +1,6 @@
 
+
+import { WebApp } from "@grammyjs/web-app";
 import TopBar from "../../components/TopBar";
 import MenuDetailSection from "./MenuDetailSection";
 import MenuHeroSection from "./MenuHeroSection";
@@ -6,28 +8,18 @@ import { useEffect, useState } from "react";
 
 
 const MainMenu = () => {
-    // const [playerInfo, setPlayerInfo] = useState(null);
+  
 
-    // useEffect(() => {
-    //   WebApp.ready();
-  
-    //   const user = WebApp.initDataUnsafe?.user;
-  
-    //   if (user) {
-    //     const playerData = {
-    //       username: user.username || 'Unknown Player',
-    //       firstName: user.first_name || 'Player',
-    //       lastName: user.last_name || '',
-    //       id: user.id,
-    //       score: 0, 
-    //     };
-  
-    //     setPlayerInfo(playerData);
-    //   }
-  
-      
-    //   WebApp.setHeaderColor('#C92A2A');
-    // }, []);
+     useEffect(() => {
+        WebApp.ready();
+        if (window.Telegram && window.Telegram.WebApp) {
+            const tg = window.Telegram.WebApp;
+          
+            tg.setHeaderColor('#C92A2A');
+          } else {
+            console.error("Telegram WebApp is not available.");
+          }
+     }, []);
   
     return (
         <div className="main-container bg-gradient-light text-white">
